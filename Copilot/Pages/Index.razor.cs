d84544c0f115f9abfd2af092d0591477ca3280d7
Kevin BEAUGRAND <contact@kbeaugrand.fr>
2023-11-21 09:36:07 +0100

    Update Demo

--- Copilot/Pages/Index.razor.cs
+++ Copilot/Pages/Index.razor.cs
@@ -2,29 +2,89 @@
 {
     using Microsoft.AspNetCore.Components;
     using Microsoft.SemanticKernel;
-    using Microsoft.SemanticKernel.CoreSkills;
+    using Microsoft.SemanticKernel.Memory;
+    using Microsoft.SemanticKernel.Plugins.Memory;
 
     public partial class Index
     {
         [Inject]
         private IKernel _kernel { get; set; }
 
+        [Inject]
+        private ISemanticTextMemory _memory { get; set; }
+
         private string _result;
 
         private string _userPrompt;
+
+        private IEnumerable<string> _languages = new List<string> { "Anglais", "Espagnol", "Français", "Allemand", "Italien" }; 
+
+        private string _language = "Français";
+
+        private async Task FindHistory()
+        {
+            _result = "Loading...";
+
+            var relatedIssues = _memory.SearchAsync("maintenance", _userPrompt, 10);
+            var result = "";
+
+            await foreach (var item in relatedIssues)
+            {
+                result += item.Metadata.Text + "\n";
+                result += "--------------------------\n";
+            }
+
+            _result = result;
+        }
+
+        private async Task Encode()
+        {
+            var context = _kernel.CreateNewContext();
+
+            context.Variables["INPUT"] = _userPrompt;
+
+            _result = "Loading...";
+
+            var solution = await _kernel.RunAsync(_kernel.Functions.GetFunction("Encoder", "AnomalyEncode"), context.Variables);
+
+            _result = solution.GetValue<string>();
+        }
+
+        private async Task GroupHistory()
+        {
+            var context = _kernel.CreateNewContext();
+
+            context.Variables["INPUT"] = _userPrompt;
+            context.Variables[TextMemoryPlugin.CollectionParam] = "maintenance";
+            context.Variables[TextMemoryPlugin.LimitParam] = "5";
+
+            _result = "Loading...";
+
+            var solution = await _kernel.RunAsync(_kernel.Functions.GetFunction("Resolver", "GroupHistory"), context.Variables);
+
+            _result = solution.GetValue<string>();
+        }
 
         private async Task FindSolution()
         {
             var context = _kernel.CreateNewContext();
 
-            context["INPUT"] = _userPrompt;
-            context[TextMemorySkill.CollectionParam] = "maintenance";
-            context[TextMemorySkill.LimitParam] = "10";
+            context.Variables["INPUT"] = _userPrompt;
+            context.Variables[TextMemoryPlugin.CollectionParam] = "maintenance";
+            context.Variables[TextMemoryPlugin.LimitParam] = "5";
+            context.Variables["LANGUAGE"] = _language;
 
-            var solution = await _kernel.Func("Resolver", "HelpMe")
-                                .InvokeAsync(context);
+            _result = "Loading...";
 
-            _result = solution.Result;
+            var solution = await _kernel.RunAsync(_kernel.Functions.GetFunction("Resolver", "HelpMe"), context.Variables);
+
+            _result = solution.GetValue<string>();
+        }
+
+
+        private void Clear()
+        {
+            _result = "";
         }
     }
 }
